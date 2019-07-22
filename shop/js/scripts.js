e93b5981518339f7dcef5e16aca1b8168b15b503
Alexandru Rusu <alexandru.rusu@softvision.ro>
2019-07-22 17:07:39 +0300

    done

--- shop/js/scripts.js
+++ shop/js/scripts.js
@@ -1,4 +1,5 @@
 var arr = [];
+var finalProdPrice = 0;
 
 document.getElementById("firstName").addEventListener("blur", function(event) {
     var value = document.getElementById("firstName").value;
@@ -35,8 +36,9 @@ document.querySelector("#firstName").addEventListener("input", function(e){
     if(!value.match(letters)){
 
         document.getElementById("firstName").setAttribute("style", "border: 2px solid red;");
-        var node = document.createTextNode("Use letters or else");
-        p.appendChild(node);
+        /* var node = document.createTextNode("Use letters or else");
+        p.appendChild(node); */
+        p.innerHTML = "error";
     }
 });
 
@@ -89,12 +91,95 @@ document.querySelector("form").addEventListener("keypress", function(){
 });
 
 
+var addButton = document.getElementsByClassName("add")
+console.log(addButton);
+for(var i = 0; i < addButton.length; i++){
+    addButton[i].onclick=addValue;
+}
+
+function addValue(e){
+    var quantity = e.target.parentElement.children[1].textContent;
+    var price = e.target.parentElement.parentElement.children[2].textContent;
+    var prodPrice = e.target.parentElement.parentElement.children[4].textContent;
+    var totalPrice = document.querySelector("#total-price").textContent;
+
+
+    console.log(quantity);
+    totalPrice = parseInt(totalPrice);
+    price = parseInt(price);
+    quantity = parseInt(quantity);
+    prodPrice = parseInt(prodPrice);
+
+    prodPrice = prodPrice + price;
+    quantity++;
+    totalPrice = totalPrice + price;
+
+    quantity = quantity.toString();
+    prodPrice = prodPrice.toString();
+    totalPrice = totalPrice.toString();
+
+    e.target.parentElement.children[1].innerHTML = quantity;
+    e.target.parentElement.parentElement.children[4].innerHTML = prodPrice;
+    document.querySelector("#total-price").innerHTML = totalPrice;
+}
+
+
+var removeButton = document.getElementsByClassName("remove")
+console.log(removeButton);
+for(var i = 0; i < removeButton.length; i++){
+    removeButton[i].onclick=removeValue;
+}
+
+function removeValue(e){
+    var quantity = e.target.parentElement.children[1].textContent;
+    var price = e.target.parentElement.parentElement.children[2].textContent;
+    var prodPrice = e.target.parentElement.parentElement.children[4].textContent;
+    var totalPrice = document.querySelector("#total-price").textContent;
+
+    totalPrice = parseInt(totalPrice);
+    prodPrice = parseInt(prodPrice);
+    price = parseInt(price);
+    quantity = parseInt(quantity);
+
+    console.log(quantity);
+
+    if(quantity === 1){
+        alert("you can't go lower than 1")
+    }else{
+        quantity--;
+        prodPrice = prodPrice - price;
+        totalPrice = totalPrice - price;
+        console.log(quantity);
+        quantity = quantity.toString();
+        e.target.parentElement.children[1].innerHTML = quantity;
+        e.target.parentElement.parentElement.children[4].innerHTML = prodPrice;
+        document.querySelector("#total-price").innerHTML = totalPrice;
+    }
+    
+}
+
+
+
 var elements = document.getElementsByClassName("remove-item");
-for(var i = 0; i <= elements.length; i++){
-    elements[i].addEventListener("click", deleteFunction, false)
+console.log("asajfajfa",elements);
+for(var i = 0; i < elements.length; i++){
+    /* elements[i].addEventListener("click", deleteFunction, false) */
+    elements[i].onclick=deleteFunction;
 }
 
 function deleteFunction(e){
+    var totalPrice = document.querySelector("#total-price").textContent;
+    var price = e.target.parentElement.parentElement.children[4].textContent;
+
+    totalPrice = parseInt(totalPrice);
+    price = parseInt(price);
+    
+    
+    totalPrice = totalPrice - price;
+
+    totalPrice = totalPrice.toString();
     e.target.parentElement.parentElement.style.display = "none";
+    document.querySelector("#total-price").innerHTML = totalPrice;
+
 
 };
